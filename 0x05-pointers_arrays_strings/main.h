c842424c63085e970e27f1f416d383953fe6551a
Kaycee27 <okelechi072@gmail.com>
2023-09-18 15:52:50 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H:
+#ifndef MAIN_H
 #define MAIN_H
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-oid _puts(char *str);
+void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
