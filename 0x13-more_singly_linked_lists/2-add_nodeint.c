06c15d45f234a818d192f9cfac8bdd08ce1b497e
Kaycee27 <okelechi072@gmail.com>
2023-10-24 06:28:51 -0700

    ftrfg

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - the function
+ * @n: node 
+ * @head: pointer
+ * Return: NULL or otherwise
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *pointer;
+
+	pointer = malloc(sizeof(listint_t));
+	if (!pointer)
+	{
+		return (NULL);
+	}
+	pointer->n = n;
+	pointer->next = *head;
+	*head = pointer;
+
+	return (pointer);
+}
